81aed6191f337d3119c63c59444614892dbb3070
Elkis Andres Rovira <earovira@enerbit.co>
2025-12-22 03:07:59 -0500

    Add new colors and grayscales to the AppColors class

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,23 +5,42 @@ class AppColors {
   static const Color primary = Color(0xFF6C63FF);
   static const Color primaryDark = Color(0xFF5A52D5);
   static const Color primaryLight = Color(0xFF8F88FF);
-  
+
   // Background colors
   static const Color background = Color(0xFFF8F9FA);
   static const Color surface = Colors.white;
   static const Color surfaceVariant = Color(0xFFF1F3F5);
-  
+
   // Text colors
   static const Color textPrimary = Color(0xFF212529);
   static const Color textSecondary = Color(0xFF6C757D);
   static const Color textTertiary = Color(0xFFADB5BD);
-  
+
   // Status colors
   static const Color error = Color(0xFFDC3545);
   static const Color success = Color(0xFF28A745);
   static const Color warning = Color(0xFFFFC107);
-  
+
   // Border colors
   static const Color border = Color(0xFFDEE2E6);
   static const Color divider = Color(0xFFE9ECEF);
+
+  // Grey scale
+  static const Color grey300 = Color(0xFFDEE2E6);
+  static const Color grey400 = Color(0xFFCED4DA);
+  static const Color grey500 = Color(0xFFADB5BD);
+  static const Color grey600 = Color(0xFF6C757D);
+  static const Color grey700 = Color(0xFF495057);
+  static const Color grey800 = Color(0xFF343A40);
+  static const Color grey = Color(0xFF6C757D);
+
+  // Error shades
+  static const Color errorLight = Color(0xFFF8D7DA);
+
+  // Additional colors
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+  static const Color black45 = Color(0x73000000);
+  static const Color black70 = Color(0xB3000000);
 }
